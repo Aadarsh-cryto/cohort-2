b0731ea651d15b68690b1e3f6335ec537db85918
Aadarsh ojha <aadarshojha46@gmail.com>
2026-01-25 12:50:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,86 +1,18 @@
 
-function getData(cb){
-    setTimeout(() => {
-        let succes = true;
-if(succes){
-    cb(null,"data recived");
-}
-else{
-    cb(null,"error ocuuured");
-}
-    }, 2000);
+function debounce(fn,delay){
+    let timer;
+    return function(){
+        clearTimeout(timer);
+        timer = setTimeout(fn,delay)
+    };
 }
 
-getData((error,result)=>{
-    if(error){
-        console.log(error);
-    }
-    else{
-        console.log(result);
-    }
-});
-
-function dukaamSeDudhLaao(cb){
-    setTimeout(() => {
-        let leaaya = true;
-        if (leaaya) {
-            cb(null,"badhiya beta shbahsh");
-        }
-        else{
-            cb(null,"kya yaar pandeyji");
-        }
-    }, 4000);
 
 
-}
-dukaamSeDudhLaao((error,result)=>{
-    if(error){
-        console.log(error);
-    }
-    else{
-        console.log(result);
-    }
-})
 
 
-function getData() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            let success = true;
-
-            if (success) {
-                resolve("Data received");
-            } else {
-                reject("Error occurred");
-            }
-        }, 2000);
-    });
-}
-getData()
-    .then((result) => {
-        console.log(result);
-    })
-    .catch((error) => {
-        console.log(error);
-    });
-
-    function dukaamSeDudhLaao(){
-        return new Promise((resolve,reject)=>{
-            setTimeout(() => {
-                let dudh = true;
-                if(dudh){
-                    console.log("data recived");
-                }
-                else{
-                    console.log("nahi");
-                }
-            }, 2000);
-        })
-    }
-    dukaamSeDudhLaao().then((result)=>{
-        console.log(result);
-    })
-    .catch((error)=>{
-        console.log("nahi")
-    })
-
+document.querySelector('#search').addEventListener("input",
+    debounce(function(){
+        console.log("chala");
+    },400)
+)
