8ce5f37d835ed58c2063781d3a29b6cd52e866f3
Aadarsh ojha <aadarshojha46@gmail.com>
2026-01-26 12:47:27 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,12 @@
-//  var btn = document.querySelector('button')
-//  var box = document.querySelector('#box')
-
-//  btn.addEventListener('click',function(){
-//     // console.log('hello')
-//     var c1 =  Math.floor(Math.random()*256)
-//      var c2 =  Math.floor(Math.random()*256)
-//      var c3 =  Math.floor(Math.random()*256)
-     
-//     box.style.backgroundColor = `rgb(${c1},${c2},${c3})`
-    
-//     box.innerHTML = `rgb(${c1},${c2},${c3})`
-
-    
-
-//  })
+var h1 = document.querySelector('h1')
+var inc = document.querySelector('button')
+var a= 0
+inc.addEventListener('click',function(){
+    a++
+        h1.innerHTML = a
+})
 
 
 
-var arr = ['ab','ac','ak','al']
-var a = Math.floor(Math.random()*arr.length)
-console.log(arr[a]);
+
+
