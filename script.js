4d2a4c5592adbb974d6f6fdf397d012912dd0bf6
Aadarsh ojha <aadarshojha46@gmail.com>
2026-02-04 12:00:53 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,55 +1,13 @@
-  var arr = [
-    {
-        team:'RCB',
-     primary:'red',
-     secondary:'black'
-    },
-    {
-        team:'CSK',
-     primary:'yellow',
-     secondary:'yellowgreen'
-
-    },
-    {
-        team:'KKR',
-     primary:'purple',
-     secondary:'gold'
-    },
-    {
-        team:'Raj',
-     primary:'pink',
-     secondary:'blue'
-    },
-    {
-        team:'SRH',
-     primary:'orange',
-     secondary:'black'
-    },
-    {
-        team:'PUN',
-     primary:'red',
-     secondary:'silver'
-    }
-  ]
-  // console.log(arr[2].team);
-  //  var a = Math.floor(Math.random()*arr.length);
-  //  console.log(arr[a]);
-    var h1 = document.querySelector('h1');
-var btn = document.querySelector('button');
-var main = document.querySelector('main')
-  
- btn.addEventListener('click',function(){
-var num = Math.floor(Math.random()*arr.length);
-// console.log(num);
-// console.log(arr[num]);
-// console.log(arr[num].team);
-// console.log(arr[num].primary);
-// console.log(arr[num].secondary);
-h1.innerHTML = arr[num].team;
-
-h1.style.color = arr[num].secondary;
-main.style.backgroundColor = arr[num].primary;
- 
+// Declare audio in advance (avoid TDZ)
 
 
- })
+const allkey = document.querySelectorAll(".piano .key");
+
+allkey.forEach((key, i) => {
+    const audio = new Audio(`piano music/key${i + 1}.mp3`);
+
+    key.onclick = () => {
+        audio.currentTime = 0;
+        audio.play();
+    };
+});
